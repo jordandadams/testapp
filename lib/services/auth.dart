eb8cdf8b766a876665e7a1b99b78f962066527d1
jordandadams <jordanadams61791@gmail.com>
2022-05-11 07:07:14 -0500

    added anon login and img to login

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -6,4 +6,19 @@ class AuthService {
   final userStream = FirebaseAuth.instance.authStateChanges();
   // used for when a user clicks on a button and you need to check their auth statement for that moment
   final user = FirebaseAuth.instance.currentUser;
+
+  // Anonymous Firebase Login
+  Future<void> anonLogin() async {
+    try {
+      await FirebaseAuth.instance.signInAnonymously();
+    } on FirebaseAuthException catch(e) {
+      //handle error
+    }
+  }
+
+  // Sign Out
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
 }
